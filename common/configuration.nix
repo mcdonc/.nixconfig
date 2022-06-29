2b337c40198302725a44879822d9c981e12d70cc
Chris McDonough <chrism@plope.com>
2022-06-29 00:55:51 -0400

    indentation

--- common/configuration.nix
+++ common/configuration.nix
@@ -309,44 +309,45 @@ in
          ls = "ls --color=auto";
       };
 
-      initExtra = ''
-         ## include config generated via "p10k configure" manually;
-       	 ## zplug cannot edit home manager's zshrc file.
+      initExtra =
+        ''
+        # be more bashy
+        setopt interactive_comments bashautolist nobeep nomenucomplete noautolist
 
-         # be more bashy
-         setopt interactive_comments bashautolist nobeep nomenucomplete noautolist
+        ## include config generated via "p10k configure" manually;
+      	 ## zplug cannot edit home manager's zshrc file.
 
-         [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+        [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
-         ## Keybindings section
-         bindkey -e
-         bindkey '^[[7~' beginning-of-line                               # Home key
-         bindkey '^[[H' beginning-of-line                                # Home key
-         if [[ "''${terminfo[khome]}" != "" ]]; then
-         bindkey "''${terminfo[khome]}" beginning-of-line                # [Home] - Go to beginning of line
-         fi
-         bindkey '^[[8~' end-of-line                                     # End key
-         bindkey '^[[F' end-of-line                                     # End key
-         if [[ "''${terminfo[kend]}" != "" ]]; then
-         bindkey "''${terminfo[kend]}" end-of-line                       # [End] - Go to end of line
-         fi
-         bindkey '^[[2~' overwrite-mode                                  # Insert key
-         bindkey '^[[3~' delete-char                                     # Delete key
-         bindkey '^[[C'  forward-char                                    # Right key
-         bindkey '^[[D'  backward-char                                   # Left key
-         bindkey '^[[5~' history-beginning-search-backward               # Page up key
-         bindkey '^[[6~' history-beginning-search-forward                # Page down key
-         # Navigate words with ctrl+arrow keys
-         bindkey '^[Oc' forward-word                                     #
-         bindkey '^[Od' backward-word                                    #
-         bindkey '^[[1;5D' backward-word                                 #
-         bindkey '^[[1;5C' forward-word                                  #
-         bindkey '^H' backward-kill-word                                 # delete previous word with ctrl+backspace
-         bindkey '^[[Z' undo                                             # Shift+tab undo last action
-         # Theming section
-         autoload -U colors
-         colors
-       '';
+        ## Keybindings section
+        bindkey -e
+        bindkey '^[[7~' beginning-of-line                               # Home key
+        bindkey '^[[H' beginning-of-line                                # Home key
+        if [[ "''${terminfo[khome]}" != "" ]]; then
+        bindkey "''${terminfo[khome]}" beginning-of-line                # [Home] - Go to beginning of line
+        fi
+        bindkey '^[[8~' end-of-line                                     # End key
+        bindkey '^[[F' end-of-line                                     # End key
+        if [[ "''${terminfo[kend]}" != "" ]]; then
+        bindkey "''${terminfo[kend]}" end-of-line                       # [End] - Go to end of line
+        fi
+        bindkey '^[[2~' overwrite-mode                                  # Insert key
+        bindkey '^[[3~' delete-char                                     # Delete key
+        bindkey '^[[C'  forward-char                                    # Right key
+        bindkey '^[[D'  backward-char                                   # Left key
+        bindkey '^[[5~' history-beginning-search-backward               # Page up key
+        bindkey '^[[6~' history-beginning-search-forward                # Page down key
+        # Navigate words with ctrl+arrow keys
+        bindkey '^[Oc' forward-word                                     #
+        bindkey '^[Od' backward-word                                    #
+        bindkey '^[[1;5D' backward-word                                 #
+        bindkey '^[[1;5C' forward-word                                  #
+        bindkey '^H' backward-kill-word                                 # delete previous word with ctrl+backspace
+        bindkey '^[[Z' undo                                             # Shift+tab undo last action
+        # Theming section
+        autoload -U colors
+        colors
+        '';
        zplug = {
          enable = true;
          plugins = [
