d0bedde64be1a61e602c2d2e16c1de5fa2d82f37
Chris McDonough <chrism@plope.com>
2022-07-20 07:54:49 -0400

    really pin kernel

--- common/rc505.nix
+++ common/rc505.nix
@@ -1,8 +1,18 @@
-{pkgs, ...}:
+{pkgs, lib, ...}:
 
 {
   # enable the Roland RC-505 as an ALSA device
-  boot.kernelPackages = pkgs.linuxPackages_5_15;
+  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_5_15.override {
+    argsOverride = rec {
+      src = pkgs.fetchurl {
+            url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
+            sha256 = "sha256-Hva9UItsOvO+8tWzN+RHclTbooTHnjKao4+XY647/cw=";
+      };
+      version = "5.15.55";
+      modDirVersion = "5.15.55";
+      };
+  });
+  # boot.kernelPackages = pkgs.linuxPackages_5_15;
   boot.kernelPatches = [{
     name = "roland-rc505";
     patch = ./roland.patch;
