527c268d377fedd4c342ec90f65e007401094791
Chris McDonough <chrism@plope.com>
2022-12-26 02:57:46 -0500

    upgrade kernel version

--- common/rc505.nix
+++ common/rc505.nix
@@ -8,10 +8,10 @@
     argsOverride = rec {
       src = pkgs.fetchurl {
         url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-        sha256 = "sha256-Hva9UItsOvO+8tWzN+RHclTbooTHnjKao4+XY647/cw=";
+        sha256 = "sha256-LAuuKfrJjgqUCJFKRVGylxNlrAAANRyyVda9mqCEmAg=";
       };
-      version = "5.15.55";
-      modDirVersion = "5.15.55";
+      version = "5.15.85";
+      modDirVersion = "5.15.85";
     };
   });
 
