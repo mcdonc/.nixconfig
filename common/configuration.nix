d885e13d79b695211978c1594f83b1f318cbd7f3
Chris McDonough <chrism@plope.com>
2023-01-08 22:47:46 -0500

    use docker

--- common/configuration.nix
+++ common/configuration.nix
@@ -165,6 +165,9 @@ in {
 
   programs.steam.enable = true;
 
+  # enable docker
+  virtualisation.docker.enable = true;
+  
   # default shell for all users
   users.defaultUserShell = pkgs.zsh;
 
@@ -172,7 +175,7 @@ in {
   users.users.chrism = {
     isNormalUser = true;
     initialPassword = "pw321";
-    extraGroups = [ "wheel" "networkmanager" "audio" ];
+    extraGroups = [ "wheel" "networkmanager" "audio" "docker" ];
     openssh = {
       authorizedKeys.keys = [
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCnLD+dQsKPhCV3eY0lMUP4fDrECI1Boe6PbnSHY+eqRpkA/Nd5okdyXvynWETivWsKdDRlT3gIVgEHqEv8s4lzxyZx9G2fAgQVVpBLk18G9wkH0ARJcJ0+RStXLy9mwYl8Bw8J6kl1+t0FE9Aa9RNtqKzpPCNJ1Uzg2VxeNIdUXawh77kIPk/6sKyT/QTNb5ruHBcd9WYyusUcOSavC9rZpfEIFF6ZhXv2FFklAwn4ggWzYzzSLJlMHzsCGmkKmTdwKijkGFR5JQ3UVY64r3SSYw09RY1TYN/vQFqTDw8RoGZVTeJ6Er/F/4xiVBlzMvxtBxkjJA9HLd8djzSKs8yf amnesia@amnesia"
