5edc9e17ca5e6102321517e7fa6e949efb1be344
Chris McDonough <chrism@plope.com>
2023-06-03 22:40:04 -0400

    verbosify swnix and friends, patch kernel on 512

--- common/rc505.nix
+++ common/rc505.nix
@@ -4,19 +4,19 @@
   # enable the Roland RC-505 as an ALSA device
 
   # pin the kernel so we don't need to keep building it
-  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_5_15.override {
+  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_6_1.override {
     argsOverride = rec {
       src = pkgs.fetchurl {
-        url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-        sha256 = "sha256-2pJw2+ZN3x2xPHBHCVf/Z5brmW2Ge7Su19FKcOHGWnI=";
+        url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
+        sha256 = "sha256-6GkXu6GZDpZ5Q2RUhBgqZLoyX5ixFKGQbMHVCZLgc8E=";
       };
-      version = "5.15.94";
-      modDirVersion = "5.15.94";
+      version = "6.1.31";
+      modDirVersion = "6.1.31";
     };
   });
 
   # this pin is not specific enough
-  # boot.kernelPackages = pkgs.linuxPackages_5_15;
+  # boot.kernelPackages = pkgs.linuxPackages_6_1;
 
   # patch the kernel
   boot.kernelPatches = [{
