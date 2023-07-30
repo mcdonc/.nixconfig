a473a086f9c379cfeecf57aa3848677cbc904f03
Chris McDonough <chrism@plope.com>
2023-07-30 15:35:32 -0400

    syntax

--- users/larry/hm.nix
+++ users/larry/hm.nix
@@ -7,7 +7,7 @@
     shell-genie
     # fd is an unnamed dependency of fzf
     fd
-];
+  ];
   home.stateVersion = "22.05";
 
   services.keybase.enable = true;
@@ -51,13 +51,6 @@
       mimeType = [ "application/vnd.olive-project" ];
       icon = "org.olivevideoeditor.Olive";
     };
-    # localxcalib = {
-    #   name = "xcalib of ~/.monprofile.icc";
-    #   genericName = "xcalib of ~/.monprofile.icc";
-    #   exec = "xcalib -S :0 ${config.home.homeDirectory}/.monprofile.icc";
-    #   categories = [ "Graphics" ];
-    #   terminal = false;
-    # };
   };
 
   # thanks to tejing on IRC for clueing me in to .force here: it will
@@ -134,67 +127,68 @@
     dotDir = ".config/zsh";
 
     sessionVariables = {
-    shellAliases = {
-      swnix = "sudo nixos-rebuild switch --verbose";
-      drynix = "sudo nixos-rebuild dry-build --verbose";
-      bootnix = "sudo nixos-rebuild boot --verbose";
-      ednix = "emacsclient -nw /etc/nixos/flake.nix";
-      schnix = "nix search nixpkgs";
-      rbnix = "sudo nixos-rebuild build --rollback";
-      replnix = "nix repl '<nixpkgs>'";
-      restartemacs = "systemctl --user restart emacs";
-      open = "kioclient exec";
-      edit = "emacsclient -n -c";
-      sgrep = "rg";
-      ls = "ls --color=auto";
-      ai = "shell-genie ask";
-    };
+      shellAliases = {
+        swnix = "sudo nixos-rebuild switch --verbose";
+        drynix = "sudo nixos-rebuild dry-build --verbose";
+        bootnix = "sudo nixos-rebuild boot --verbose";
+        ednix = "emacsclient -nw /etc/nixos/flake.nix";
+        schnix = "nix search nixpkgs";
+        rbnix = "sudo nixos-rebuild build --rollback";
+        replnix = "nix repl '<nixpkgs>'";
+        restartemacs = "systemctl --user restart emacs";
+        open = "kioclient exec";
+        edit = "emacsclient -n -c";
+        sgrep = "rg";
+        ls = "ls --color=auto";
+        ai = "shell-genie ask";
+      };
 
-    initExtra = ''
-      # be more bashy
-      setopt interactive_comments bashautolist nobeep nomenucomplete noautolist
+      initExtra = ''
+        # be more bashy
+        setopt interactive_comments bashautolist nobeep nomenucomplete noautolist
 
-      ## include config generated via "p10k configure" manually;
-      ## zplug cannot edit home manager's zshrc file.
+        ## include config generated via "p10k configure" manually;
+        ## zplug cannot edit home manager's zshrc file.
 
-      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+        [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
-      ## Keybindings section
-      bindkey -e
-      bindkey '^I' fzf-completion                         # anything**<TAB>
-      bindkey '^[[7~' beginning-of-line                               # Home key
-      bindkey '^[[H' beginning-of-line                                # Home key
-      if [[ "''${terminfo[khome]}" != "" ]]; then
-      bindkey "''${terminfo[khome]}" beginning-of-line                # [Home] - Go to beginning of line
-      fi
-      bindkey '^[[8~' end-of-line                                     # End key
-      bindkey '^[[F' end-of-line                                     # End key
-      if [[ "''${terminfo[kend]}" != "" ]]; then
-      bindkey "''${terminfo[kend]}" end-of-line                       # [End] - Go to end of line
-      fi
-      bindkey '^[[2~' overwrite-mode                                  # Insert key
-      bindkey '^[[3~' delete-char                                     # Delete key
-      bindkey '^[[C'  forward-char                                    # Right key
-      bindkey '^[[D'  backward-char                                   # Left key
-      bindkey '^[[5~' history-beginning-search-backward               # Page up key
-      bindkey '^[[6~' history-beginning-search-forward                # Page down key
-      # Navigate words with ctrl+arrow keys
-      bindkey '^[Oc' forward-word                                     #
-      bindkey '^[Od' backward-word                                    #
-      bindkey '^[[1;5D' backward-word                                 #
-      bindkey '^[[1;5C' forward-word                                  #
-      bindkey '^H' backward-kill-word                                 # delete previous word with ctrl+backspace
-      bindkey '^[[Z' undo                                             # Shift+tab undo last action
-      # Theming section
-      autoload -U colors
-      colors
-    '';
-    zplug = {
-      enable = true;
-      plugins = [{
-        name = "romkatv/powerlevel10k";
-        tags = [ "as:theme" "depth:1" ];
-      }];
+        ## Keybindings section
+        bindkey -e
+        bindkey '^I' fzf-completion                         # anything**<TAB>
+        bindkey '^[[7~' beginning-of-line                               # Home key
+        bindkey '^[[H' beginning-of-line                                # Home key
+        if [[ "''${terminfo[khome]}" != "" ]]; then
+        bindkey "''${terminfo[khome]}" beginning-of-line                # [Home] - Go to beginning of line
+        fi
+        bindkey '^[[8~' end-of-line                                     # End key
+        bindkey '^[[F' end-of-line                                     # End key
+        if [[ "''${terminfo[kend]}" != "" ]]; then
+        bindkey "''${terminfo[kend]}" end-of-line                       # [End] - Go to end of line
+        fi
+        bindkey '^[[2~' overwrite-mode                                  # Insert key
+        bindkey '^[[3~' delete-char                                     # Delete key
+        bindkey '^[[C'  forward-char                                    # Right key
+        bindkey '^[[D'  backward-char                                   # Left key
+        bindkey '^[[5~' history-beginning-search-backward               # Page up key
+        bindkey '^[[6~' history-beginning-search-forward                # Page down key
+        # Navigate words with ctrl+arrow keys
+        bindkey '^[Oc' forward-word                                     #
+        bindkey '^[Od' backward-word                                    #
+        bindkey '^[[1;5D' backward-word                                 #
+        bindkey '^[[1;5C' forward-word                                  #
+        bindkey '^H' backward-kill-word                                 # delete previous word with ctrl+backspace
+        bindkey '^[[Z' undo                                             # Shift+tab undo last action
+        # Theming section
+        autoload -U colors
+        colors
+      '';
+      zplug = {
+        enable = true;
+        plugins = [{
+          name = "romkatv/powerlevel10k";
+          tags = [ "as:theme" "depth:1" ];
+        }];
+      };
     };
   };
 }
