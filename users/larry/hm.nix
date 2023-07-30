859109cfaa6361f4bcf3eb09c9c01c29730e790f
Chris McDonough <chrism@plope.com>
2023-07-30 15:44:14 -0400

    replace larry config with newer from chrism

--- users/larry/hm.nix
+++ users/larry/hm.nix
@@ -1,17 +1,143 @@
-{ config, pkgs, ... }:
+{ config, pkgs, home-manager, ... }:
 
-{
+let
+  gterm-change-profile = "xdotool key --clearmodifiers Shift+F10 r";
+  ssh-chcolor = pkgs.writeShellScriptBin "ssh-chcolor" ''
+    function chcolor(){
+      # emulates right-clicking and selecting a numbered gnome-terminal profile.
+      # hide output if it fails (if you're ssh'ed in, for example, and
+      # use ssh recursively).  --clearmodifiers ignores any
+      # modifier keys you're physically holding before sending the command
+      if [ -n "$GNOME_TERMINAL_SERVICE" ]; then
+         ${gterm-change-profile} $1 > /dev/null 2>&1
+      fi
+    }
+    chcolor 4; ssh $@; chcolor 1
+  '';
+  defaultpalette = [
+    "#171421"
+    "#ED1515"
+    "#11D116"
+    "#FF6D03"
+    "#1D99F3"
+    "#A347BA"
+    "#2AA1B3"
+    "#D0CFCC"
+    "#5E5C64"
+    "#F66151"
+    "#33D17A"
+    "#E9AD0C"
+    "#2A7BDE"
+    "#C061CB"
+    "#33C7DE"
+    "#FFFFFF"
+  ];
+  defaultprofile = {
+    default = true;
+    visibleName = "1grey";
 
+    scrollbackLines = 10485760; # null is meant to mean infinite but no work
+    showScrollbar = true;
+    scrollOnOutput = false;
+    font = "Ubuntu Mono 18";
+    boldIsBright = true;
+    audibleBell = false;
+
+    colors = {
+      palette = defaultpalette;
+      backgroundColor = "#1C2023";
+      foregroundColor = "#FFFFFF";
+    };
+  };
+
+in {
   home.packages = with pkgs; [
     keybase-gui
-    #shell-genie
+    ssh-chcolor
+    xdotool
     # fd is an unnamed dependency of fzf
     fd
   ];
   home.stateVersion = "22.05";
 
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Breeze-Dark";
+      package = pkgs.libsForQt5.breeze-gtk;
+    };
+  };
+
+  programs.gnome-terminal = {
+    enable = true;
+    showMenubar = false;
+
+    profile.b1dcc9dd-5262-4d8d-a863-c897e6d979b9 = defaultprofile;
+    profile.ec7087d3-ca76-46c3-a8ec-aba2f3a65db7 = defaultprofile // {
+      default = false;
+      visibleName = "2blue";
+      colors = {
+        palette = defaultpalette;
+        backgroundColor = "#00008E";
+        foregroundColor = "#D0CFCC";
+      };
+    };
+    profile.ea1f3ac4-cfca-4fc1-bba7-fdf26666d188 = defaultprofile // {
+      default = false;
+      visibleName = "3black";
+      colors = {
+        palette = defaultpalette;
+        backgroundColor = "#000000";
+        foregroundColor = "#D0CFCC";
+      };
+    };
+    profile.a37ed5e4-99f5-4eba-acef-e491965a6076 = defaultprofile // {
+      default = false;
+      visibleName = "4purple";
+      colors = {
+        palette = defaultpalette;
+        backgroundColor = "#2C0035";
+        foregroundColor = "#D0CFCC";
+      };
+    };
+  };
+
   services.keybase.enable = true;
   services.kbfs.enable = true;
+  services.gpg-agent = {
+    enable = true;
+    defaultCacheTtl = 1800;
+    enableSshSupport = true;
+  };
+
+  xdg.configFile."black".text = ''
+    [tool.black]
+    line-length = 80
+  '';
+
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "lock802" = {
+        user = "pi";
+        forwardAgent = true;
+      };
+      "testlock802" = {
+        user = "pi";
+        forwardAgent = true;
+      };
+      "testlock802lan" = {
+        user = "pi";
+        forwardAgent = true;
+      };
+
+      "win10" = { user = "user"; };
+      "thinknix*" = { forwardAgent = true; };
+      "bouncer.repoze.org" = { forwardAgent = true; };
+      "lock802.repoze.org" = { forwardAgent = true; };
+      "192.168.1.1" = { user = "root"; };
+    };
+  };
 
   xdg.configFile."environment.d/ssh_askpass.conf".text = ''
     SSH_ASKPASS="${pkgs.ksshaskpass}/bin/ksshaskpass"
@@ -38,6 +164,35 @@
       [ -z "$SSH_AGENT_PID" ] || eval "$(ssh-agent -k)"
     '';
     executable = true;
+  };
+
+  xdg.configFile."mpv/input.conf" = {
+    text = ''
+      PGDWN osd-msg-bar seek 5 exact
+      PGUP osd-msg-bar seek -5
+      Shift+PGDWN osd-msg-bar seek 30 exact
+      Shift+PGUP osd-msg-bar seek -30 exact
+      RIGHT osd-msg-bar seek 1 exact
+      LEFT osd-msg-bar seek -1 exact
+      Shift+RIGHT osd-msg-bar seek 1 exact
+      Shift+LEFT osd-msg-bar seek -1 exact
+      UP add volume 2
+      DOWN add volume -2
+      n playlist-next
+      p playlist-prev
+    '';
+  };
+
+  xdg.configFile."mpv/mpv.conf" = {
+    text = ''
+      osd-level=2
+      volume=20
+      volume-max=150
+      autofit=100%x98%
+      geometry=+50%-25
+      #window-maximized
+      # see https://github.com/mpv-player/mpv/issues/10229
+    '';
   };
 
   # add Olive for nvidia-offload (as installed per video)
@@ -116,9 +271,11 @@
     source = ../steam.desktop;
   };
 
+  programs.gitui.enable = true;
+
   programs.dircolors.enable = true;
-  #programs.fzf.enable = true;
-  #programs.fzf.enableZshIntegration = true;
+  programs.fzf.enable = true;
+  programs.fzf.enableZshIntegration = true;
 
   programs.zsh = {
     enable = true;
@@ -127,68 +284,93 @@
     dotDir = ".config/zsh";
 
     sessionVariables = {
-      shellAliases = {
-        swnix = "sudo nixos-rebuild switch --verbose";
-        drynix = "sudo nixos-rebuild dry-build --verbose";
-        bootnix = "sudo nixos-rebuild boot --verbose";
-        ednix = "emacsclient -nw /etc/nixos/flake.nix";
-        schnix = "nix search nixpkgs";
-        rbnix = "sudo nixos-rebuild build --rollback";
-        replnix = "nix repl '<nixpkgs>'";
-        restartemacs = "systemctl --user restart emacs";
-        open = "kioclient exec";
-        edit = "emacsclient -n -c";
-        sgrep = "rg";
-        ls = "ls --color=auto";
-        ai = "shell-genie ask";
-      };
+      EDITOR = "vi";
+      DSSI_PATH =
+        "$HOME/.dssi:$HOME/.nix-profile/lib/dssi:/run/current-system/sw/lib/dssi";
+      LADSPA_PATH =
+        "$HOME/.ladspa:$HOME/.nix-profile/lib/ladspa:/run/current-system/sw/lib/ladspa";
+      LV2_PATH = 
+        "$HOME/.lv2:$HOME/.nix-profile/lib/lv2:/run/current-system/sw/lib/lv2";
+      LXVST_PATH =
+        "$HOME/.lxvst:$HOME/.nix-profile/lib/lxvst:/run/current-system/sw/lib/lxvst";
+      VST_PATH =
+        "$HOME/.vst:$HOME/.nix-profile/lib/vst:/run/current-system/sw/lib/vst";
+      VST3_PATH =
+        "$HOME/.vst3:$HOME/.nix-profile/lib/vst3:/run/current-system/sw/lib/vst3";
+    };
 
-      initExtra = ''
-        # be more bashy
-        setopt interactive_comments bashautolist nobeep nomenucomplete noautolist
+    shellAliases = {
+      swnix = "sudo nixos-rebuild switch --verbose";
+      drynix = "sudo nixos-rebuild dry-build --verbose";
+      bootnix = "sudo nixos-rebuild boot --verbose";
+      ednix = "emacsclient -nw /etc/nixos/flake.nix";
+      schnix = "nix search nixpkgs";
+      rbnix = "sudo nixos-rebuild build --rollback";
+      replnix = "nix repl '<nixpkgs>'";
+      restartemacs = "systemctl --user restart emacs";
+      open = "kioclient exec";
+      edit = "emacsclient -n -c";
+      sgrep = "rg";
+      ls = "ls --color=auto";
+      greyterm = "${gterm-change-profile} 1";
+      blueterm = "${gterm-change-profile} 2";
+      blackterm = "${gterm-change-profile} 3";
+      purpleterm = "${gterm-change-profile} 4";
+      ssh = "${ssh-chcolor}/bin/ssh-chcolor";
+    };
 
-        ## include config generated via "p10k configure" manually;
-        ## zplug cannot edit home manager's zshrc file.
+    completionInit = ""; # speed up zsh start time
 
-        [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+    #initExtraFirst = ''
+    # zmodload zsh/zprof
+    #'';
 
-        ## Keybindings section
-        bindkey -e
-        bindkey '^I' fzf-completion                         # anything**<TAB>
-        bindkey '^[[7~' beginning-of-line                               # Home key
-        bindkey '^[[H' beginning-of-line                                # Home key
-        if [[ "''${terminfo[khome]}" != "" ]]; then
-        bindkey "''${terminfo[khome]}" beginning-of-line                # [Home] - Go to beginning of line
-        fi
-        bindkey '^[[8~' end-of-line                                     # End key
-        bindkey '^[[F' end-of-line                                     # End key
-        if [[ "''${terminfo[kend]}" != "" ]]; then
-        bindkey "''${terminfo[kend]}" end-of-line                       # [End] - Go to end of line
-        fi
-        bindkey '^[[2~' overwrite-mode                                  # Insert key
-        bindkey '^[[3~' delete-char                                     # Delete key
-        bindkey '^[[C'  forward-char                                    # Right key
-        bindkey '^[[D'  backward-char                                   # Left key
-        bindkey '^[[5~' history-beginning-search-backward               # Page up key
-        bindkey '^[[6~' history-beginning-search-forward                # Page down key
-        # Navigate words with ctrl+arrow keys
-        bindkey '^[Oc' forward-word                                     #
-        bindkey '^[Od' backward-word                                    #
-        bindkey '^[[1;5D' backward-word                                 #
-        bindkey '^[[1;5C' forward-word                                  #
-        bindkey '^H' backward-kill-word                                 # delete previous word with ctrl+backspace
-        bindkey '^[[Z' undo                                             # Shift+tab undo last action
-        # Theming section
-        autoload -U colors
-        colors
-      '';
-      zplug = {
-        enable = true;
-        plugins = [{
-          name = "romkatv/powerlevel10k";
-          tags = [ "as:theme" "depth:1" ];
-        }];
-      };
+    initExtra = ''
+      # be more bashy
+      setopt interactive_comments bashautolist nobeep nomenucomplete noautolist
+
+
+      ## include config generated via "p10k configure" manually;
+      ## zplug cannot edit home manager's zshrc file.
+
+      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+      ## Keybindings section
+      bindkey -e
+      bindkey '^I' fzf-completion                         # anything**<TAB>
+      bindkey '^[[7~' beginning-of-line                   # Home key
+      bindkey '^[[H' beginning-of-line                    # Home key
+      # [Home] - Go to beginning of line
+      if [[ "''${terminfo[khome]}" != "" ]]; then
+      bindkey "''${terminfo[khome]}" beginning-of-line
+      fi
+      bindkey '^[[8~' end-of-line                         # End key
+      bindkey '^[[F' end-of-line                          # End key
+      # [End] - Go to end of line
+      if [[ "''${terminfo[kend]}" != "" ]]; then
+      bindkey "''${terminfo[kend]}" end-of-line
+      fi
+      bindkey '^[[2~' overwrite-mode                      # Insert key
+      bindkey '^[[3~' delete-char                         # Delete key
+      bindkey '^[[C'  forward-char                        # Right key
+      bindkey '^[[D'  backward-char                       # Left key
+      bindkey '^[[5~' history-beginning-search-backward   # Page up key
+      bindkey '^[[6~' history-beginning-search-forward    # Page down key
+      # Navigate words with ctrl+arrow keys
+      bindkey '^[Oc' forward-word
+      bindkey '^[Od' backward-word
+      bindkey '^[[1;5D' backward-word
+      bindkey '^[[1;5C' forward-word
+      # delete previous word with ctrl+backspace
+      bindkey '^H' backward-kill-word
+      #zprof
+    '';
+    zplug = {
+      enable = true;
+      plugins = [{
+        name = "romkatv/powerlevel10k";
+        tags = [ "as:theme" "depth:1" ];
+      }];
     };
   };
 }
