c0a686591feee35f90e18e98f6e6b785e35de157
Chris McDonough <chrism@plope.com>
2023-08-01 12:25:11 -0400

    try to encrypt dns

--- common/configuration.nix
+++ common/configuration.nix
@@ -105,6 +105,20 @@
   networking.networkmanager.enable = true;
   networking.firewall.enable = false;
 
+  # encrypt dns (both networking.nameservers and services.resolved)
+  networking.nameservers =
+    [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    extraConfig = ''
+      DNSOverTLS=yes
+    '';
+  };
+
   time.timeZone = "America/New_York";
 
   hardware.bluetooth.enable = true;
