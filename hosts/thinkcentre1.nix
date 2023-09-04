dd34ac2408af523785b7f9362e71308d0b70624a
Chris McDonough <chrism@plope.com>
2023-09-04 00:18:58 -0400

    consolidate

--- hosts/thinkcentre1.nix
+++ hosts/thinkcentre1.nix
@@ -3,7 +3,6 @@
 {
   imports = [
     #"${nixos-hardware}/lenovo/thinkpad/p51"
-    ../thinkcentre.nix
     ../encryptedzfs.nix
     ../sessile.nix
     ../common.nix
@@ -12,9 +11,6 @@
 
   networking.hostId = "eeabbced";
   networking.hostName = "thinkcentre1";
-
-  hardware.nvidia.prime.offload.enable = false;
-  hardware.nvidia.prime.sync.enable = lib.mkForce true;
 
   # silence ACPI "errors" at boot shown before NixOS stage 1 output (default
   # is 4)
@@ -25,4 +21,31 @@
   #systemd.services.NetworkManager-wait-online.enable = false;
 
   #services.cachix-agent.enable = true;
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" "sr_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  hardware = {
+    nvidia = {
+      modesetting.enable = true;
+      powerManagement.enable = true;
+      open = false;
+      nvidiaSettings = false;
+    };
+
+    # is this too much?  It's convenient for Steam.
+    opengl = {
+      enable = true;
+      driSupport = lib.mkDefault true;
+      driSupport32Bit = lib.mkDefault true;
+    };
+  };
+
+  # required to make wireless work
+  hardware.enableAllFirmware = lib.mkDefault true;
 }
