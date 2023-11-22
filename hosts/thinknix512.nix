39d5815c7331c99f04c1dec079aa4d19c1ec717d
Chris McDonough <chrism@plope.com>
2023-11-22 14:02:17 -0500

    only encrypt dns on thinknix512

--- hosts/thinknix512.nix
+++ hosts/thinknix512.nix
@@ -126,4 +126,18 @@ in
     };
   };
 
+  # encrypt dns (both networking.nameservers and services.resolved)
+  networking.nameservers =
+    [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    extraConfig = ''
+      DNSOverTLS=yes
+    '';
+  };
+
 }
