bbbc09a4eda57bee0e9cf463feb2493fb8baddfb
Chris McDonough <chrism@plope.com>
2023-12-01 23:19:38 -0500

    use older nvidia driver

--- hosts/thinknix512.nix
+++ hosts/thinknix512.nix
@@ -21,6 +21,7 @@ in
     ../sessile.nix
 #    ../rc505
     ../common.nix
+    ../oldnvidia.nix # targeting 535.129.03, 545.29.02 backlightrestore doesn't work
   ];
   system.stateVersion = "22.05";
 
