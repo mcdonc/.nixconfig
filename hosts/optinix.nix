6dafa5d27ab98d612821a28d8e6842ba87e90f7a
Chris McDonough <chrism@plope.com>
2023-12-04 01:29:43 -0500

    add a /nix fileSystem to optinix

--- hosts/optinix.nix
+++ hosts/optinix.nix
@@ -1,10 +1,12 @@
-{ config, pkgs, lib, nixos-hardware, ... }:
-{
-  imports = [
-    "${nixos-hardware}/common/cpu/intel"
-    ../encryptedzfs.nix
-    ../common.nix
-  ];
+{ config, pkgs, lib, nixos-hardware, ... }: {
+  imports =
+    [ "${nixos-hardware}/common/cpu/intel" ../encryptedzfs.nix ../common.nix ];
+
+  fileSystems."/nix" = {
+    device = "NIXROOT/home";
+    fsType = "nix";
+  };
+  
   system.stateVersion = "23.11";
 
   networking.hostId = "0a2c6440b";
