1496a9eb0c52656717e382f76752480a8f1c9bff
Chris McDonough <chrism@plope.com>
2023-12-04 21:51:49 -0500

    iterate

--- hosts/profiles/backupsource/restrictbackup.py
+++ hosts/profiles/backupsource/restrictbackup.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 import os
 
-f = open('/tmp/commands', 'a')
-original = os.environ['SSH_ORIGINAL_COMMAND']
-
-f.write(original + '\n')
-print(original)
-
 allowed = """exit
-echo -n
-command -v lzop
-command -v mbuffer
-zpool get -o value -H feature@extensible_dataset 'NIXROOT'
-zfs get -H syncoid:sync 'NIXROOT/home'
-zfs get -H syncoid:sync 'NIXROOT/root'
+echo
+command
+zpool
+zfs
 """.split()
+
+if __name__ == "__main__":
+    original = os.environ['SSH_ORIGINAL_COMMAND']
+
+    f = open('/tmp/commands', 'a')
+
+    f.write(original + '\n')
+    print(original)
+    for name in allowed:
+        if original.startswith(name):
+            result = os.popen(original).read()
+            f.write(result+"\n")
+            print(result)
