bd906d11a28949c52a448392ff9f94c8fea5cf35
Chris McDonough <chrism@plope.com>
2023-12-05 06:20:11 -0500

    get rid of swap

--- hosts/optinix.nix
+++ hosts/optinix.nix
@@ -27,7 +27,7 @@
     fsType = "zfs";
   };
 
-  swapDevices = [{ device = "/dev/zvol/NIXROOT/swap"; }];
+  #swapDevices = [{ device = "/dev/zvol/NIXROOT/swap"; }];
   system.stateVersion = "23.11";
 
   networking.hostId = "0a2c6440";
