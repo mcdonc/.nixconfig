964f733d75a3c8f0f12e99b1b7791aef9cbebe21
Chris McDonough <chrism@plope.com>
2023-12-08 22:37:31 -0500

    factor out grub

--- hosts/profiles/encryptedzfs.nix
+++ hosts/profiles/encryptedzfs.nix
@@ -1,21 +1,10 @@
 { ... }:
 
 {
-  # Use GRUB, assume UEFI
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.devices = [ "nodev" ];
-  boot.loader.grub.efiInstallAsRemovable = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.splashImage = ./grub/alwaysnix.png;
-  boot.loader.grub.splashMode = "stretch"; # "normal"
-  boot.loader.grub.useOSProber = true;
-  boot.loader.timeout = 60;
+  imports = [
+    ./grub/efi.nix
+  ];
 
-  # copyKernels: "Using NixOS on a ZFS root file system might result in the
-  # boot error external pointer tables not supported when the number of
-  # hardlinks in the nix store gets very high.
-  boot.loader.grub.copyKernels = true;
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.requestEncryptionCredentials = true;
   # 4 GB max ARC cache
---
+++ hosts/profiles/grub/efi.nix
@@ -0,0 +1,20 @@
+{ ... }:
+
+{
+  # Use GRUB, assume UEFI
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.devices = [ "nodev" ];
+  boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.splashImage = ./alwaysnix.png;
+  boot.loader.grub.splashMode = "stretch"; # "normal"
+  boot.loader.grub.useOSProber = true;
+  boot.loader.timeout = 60;
+
+  # copyKernels: "Using NixOS on a ZFS root file system might result in the
+  # boot error external pointer tables not supported when the number of
+  # hardlinks in the nix store gets very high.
+  boot.loader.grub.copyKernels = true;
+
+}
