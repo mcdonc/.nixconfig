9ffc186d136ff0ae88ff0db5923951419ddbecba
Chris McDonough <chrism@plope.com>
2024-01-03 11:06:59 -0500

    filter out dirs below globally excluded

--- etc/findnixstorelinks.py
+++ etc/findnixstorelinks.py
@@ -1,14 +1,23 @@
 import os
 home = os.path.expanduser('~')
+prevent = [
+    os.path.join('.', '.cache'),
+    os.path.join('.', '.local', 'share', 'Steam'),
+    os.path.join('.', '.local', 'share', 'keybase'),
+    os.path.join('.', '.local', 'share', 'baloo'),
+    ]
+for path in prevent:
+    print(path)
 for (root, dirs, files) in os.walk(home):
     for f in files:
         full = os.path.join(root, f)
         if os.path.islink(full):
             real = os.path.realpath(full)
             if real.startswith('/nix'):
-                print('.' + full[len(home):])
-print(os.path.join('.', '.cache'))
-print(os.path.join('.', '.local', 'share', 'Steam'))
-print(os.path.join('.', '.local', 'share', 'keybase'))
-print(os.path.join('.', '.local', 'share', 'baloo'))
+                rel = '.' + full[len(home):]
+                for p in prevent:
+                    if rel.startswith(p):
+                        break
+                else: # nobreak
+                    print(rel)
 # tar cvzf /backup/location.tar.gz --exclude-from=<output of this script>
