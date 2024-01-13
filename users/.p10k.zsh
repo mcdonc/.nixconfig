56cc7fbabada86a9ce78be25f80aebc79a9acd7f
Chris McDonough <chrism@plope.com>
2024-01-13 12:28:22 -0500

    use same-dir

--- users/.p10k.zsh
+++ users/.p10k.zsh
@@ -1708,7 +1708,7 @@
   #   - always:   Trim down prompt when accepting a command line.
   #   - same-dir: Trim down prompt when accepting a command line unless this is the first command
   #               typed after changing current working directory.
-  typeset -g POWERLEVEL9K_TRANSIENT_PROMPT=off
+  typeset -g POWERLEVEL9K_TRANSIENT_PROMPT=same-dir
 
   # Instant prompt mode.
   #
