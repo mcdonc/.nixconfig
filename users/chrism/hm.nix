31b943f50cccc7d6ec4a28a46356dff899791b39
Chris McDonough <chrism@plope.com>
2024-01-24 01:09:53 -0500

    format comment

--- users/chrism/hm.nix
+++ users/chrism/hm.nix
@@ -446,15 +446,19 @@ in {
     # https://medium.com/@dannysmith/little-thing-2-speeding-up-zsh-f1860390f92
     # (needs extended_glob)
     completionInit = ''
-      # On slow systems, checking the cached .zcompdump file to see if it must be
-      # regenerated adds a noticable delay to zsh startup.  This little hack restricts
-      # it to once a day.  It should be pasted into your own completion file.
+      # On slow systems, checking the cached .zcompdump file to see if it must
+      # be regenerated adds a noticable delay to zsh startup.  This little
+      # hack restricts it to once a day.  It should be pasted into your own
+      # completion file.
       #
       # The globbing is a little complicated here:
-      # - '#q' is an explicit glob qualifier that makes globbing work within zsh's [[ ]] construct.
-      # - 'N' makes the glob pattern evaluate to nothing when it doesn't match (rather than throw a globbing error)
+      # - '#q' is an explicit glob qualifier that makes globbing work within
+      #   zsh's [[ ]] construct.
+      # - 'N' makes the glob pattern evaluate to nothing when it doesn't match
+      #   (rather than throw a globbing error)
       # - '.' matches "regular files"
-      # - 'mh+24' matches files (or directories or whatever) that are older than 24 hours.
+      # - 'mh+24' matches files (or directories or whatever) that are older
+      #   than 24 hours.
       setopt extended_glob
       autoload -Uz compinit
       export ZDOTDIR=~/${zshDotDir}
