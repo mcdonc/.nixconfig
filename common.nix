0359016b2007da201bf463ebedb04b4dde01f67a
Chris McDonough <chrism@plope.com>
2024-01-25 18:55:02 -0500

    add loccount

--- common.nix
+++ common.nix
@@ -407,6 +407,7 @@ in {
     appimage-run
     jq
     gnome.gucharmap
+    loccount
     # https://github.com/WolfangAukang/nur-packages/issues/9#issuecomment-1089072988
     # share/vdhcoapp/net.downloadhelper.coapp install --user
     #config.nur.repos.wolfangaukang.vdhcoapp
