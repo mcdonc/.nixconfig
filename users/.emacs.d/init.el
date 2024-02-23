0b7d1f656511bb833a68d228d788df8dbdef35ed
Chris McDonough <chrism@plope.com>
2024-02-23 06:16:24 -0500

    fix markdown pre text font size

--- users/.emacs.d/init.el
+++ users/.emacs.d/init.el
@@ -313,3 +313,16 @@
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ ;; (this is to fix markdown mode preformatted text font size)
+ '(fixed-pitch ((t (:family "Ubuntu Nerd Font Mono")))))
