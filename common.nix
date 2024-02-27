72d9af9c0365a5a9dfd86077128a03ffbca8079e
Chris McDonough <chrism@plope.com>
2024-02-27 17:22:38 -0500

    run aarch64-linux and add rpi2 script

--- common.nix
+++ common.nix
@@ -246,14 +246,19 @@ in
   '';
 
   # run appimages directly (see https://nixos.wiki/wiki/Appimage)
-  boot.binfmt.registrations.appimage = {
-    wrapInterpreterInShell = false;
-    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
-    recognitionType = "magic";
-    offset = 0;
-    mask = "\\xff\\xff\\xff\\xff\\x00\\x00\\x00\\x00\\xff\\xff\\xff";
-    magicOrExtension = "\\x7fELF....AI\\x02";
+  boot.binfmt = {
+    registrations.appimage = {
+      wrapInterpreterInShell = false;
+      interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+      recognitionType = "magic";
+      offset = 0;
+      mask = "\\xff\\xff\\xff\\xff\\x00\\x00\\x00\\x00\\xff\\xff\\xff";
+      magicOrExtension = "\\x7fELF....AI\\x02";
+    };
+    # run aarch64 binaries
+    emulatedSystems = [ "aarch64-linux" ];
   };
+
 
   environment.systemPackages = with pkgs; [
     cachix
