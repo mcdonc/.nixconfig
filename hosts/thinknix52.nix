3b660168154975090f00714c8bb5a647097d8fa0
Chris McDonough <chrism@plope.com>
2024-03-20 01:12:39 -0400

    offload mode

--- hosts/thinknix52.nix
+++ hosts/thinknix52.nix
@@ -15,8 +15,8 @@
   system.stateVersion = "22.05";
 
   # override optimus default offload mode to deal with external monitor
-  hardware.nvidia.prime.offload.enable = false;
-  hardware.nvidia.prime.sync.enable = true;
+  hardware.nvidia.prime.offload.enable = true;
+  hardware.nvidia.prime.sync.enable = false;
 
   # silence ACPI "errors" at boot shown before NixOS stage 1 output (default
   # is 4)
