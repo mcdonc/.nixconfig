fa38a588108e18429525737cca4f3ccabeb2d5b6
Chris McDonough <chrism@plope.com>
2024-03-28 06:52:04 -0400

    use 23.11 keybase

--- users/keybase.nix
+++ users/keybase.nix
@@ -1,48 +1,12 @@
 { pkgs, pkgs-kb-bumpversion, ... }:
 
+let
+  # switch to pkgs-kb-bumpversion and switch to explicit config
+  # if kb-pkgs craps out
+  kb-pkgs = pkgs;
+in
+
 {
-  #services.keybase.enable = true;
-  #services.kbfs.enable = true;
-
-  home.packages = with pkgs-kb-bumpversion; [
-    keybase
-    keybase-gui
-  ];
-
-  systemd.user.services.keybase = {
-    Unit.Description = "Keybase service";
-
-    Service = {
-      ExecStart =
-        "${pkgs-kb-bumpversion.keybase}/bin/keybase service --auto-forked";
-      Restart = "on-failure";
-      PrivateTmp = true;
-    };
-
-    Install.WantedBy = [ "default.target" ];
-  };
-
-  systemd.user.services.kbfs = {
-    Unit = {
-      Description = "Keybase File System";
-      Requires = [ "keybase.service" ];
-      After = [ "keybase.service" ];
-    };
-
-    Service =
-      let mountPoint = ''"%h/keybase"'';
-      in {
-        Environment = "PATH=/run/wrappers/bin KEYBASE_SYSTEMD=1";
-        ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ${mountPoint}";
-        ExecStart = "${pkgs-kb-bumpversion.kbfs}/bin/kbfsfuse ${mountPoint}";
-        ExecStopPost = "/run/wrappers/bin/fusermount -u ${mountPoint}";
-        Restart = "on-failure";
-        PrivateTmp = true;
-      };
-
-    Install.WantedBy = [ "default.target" ];
-  };
-
   # thanks to tejing on IRC for clueing me in to .force here: it will
   # overwrite any existing file.
   xdg.configFile."autostart/keybase_autostart.desktop".force = true;
@@ -54,7 +18,7 @@
     [Desktop Entry]
     Comment[en_US]=Keybase Filesystem Service and GUI
     Comment=Keybase Filesystem Service and GUI
-    Exec=env KEYBASE_AUTOSTART=1 ${pkgs-kb-bumpversion.keybase-gui}/bin/keybase-gui --disable-gpu-sandbox
+    Exec=env KEYBASE_AUTOSTART=1 ${kb-pkgs.keybase-gui}/bin/keybase-gui --disable-gpu-sandbox
     GenericName[en_US]=
     GenericName=
     MimeType=
@@ -69,6 +33,48 @@
     X-KDE-SubstituteUID=false
     X-KDE-Username=
   '';
+
+  home.packages = with kb-pkgs; [
+    keybase-gui
+    keybase
+  ];
+
+  services.keybase.enable = true;
+  services.kbfs.enable = true;
+
+  # systemd.user.services.keybase = {
+  #   Unit.Description = "Keybase service";
+
+  #   Service = {
+  #     ExecStart =
+  #       "${kb-pkgs.keybase}/bin/keybase service --auto-forked";
+  #     Restart = "on-failure";
+  #     PrivateTmp = true;
+  #   };
+
+  #   Install.WantedBy = [ "default.target" ];
+  # };
+
+  # systemd.user.services.kbfs = {
+  #   Unit = {
+  #     Description = "Keybase File System";
+  #     Requires = [ "keybase.service" ];
+  #     After = [ "keybase.service" ];
+  #   };
+
+  #   Service =
+  #     let mountPoint = ''"%h/keybase"'';
+  #     in {
+  #       Environment = "PATH=/run/wrappers/bin KEYBASE_SYSTEMD=1";
+  #       ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ${mountPoint}";
+  #       ExecStart = "${kb-pkgs.kbfs}/bin/kbfsfuse ${mountPoint}";
+  #       ExecStopPost = "/run/wrappers/bin/fusermount -u ${mountPoint}";
+  #       Restart = "on-failure";
+  #       PrivateTmp = true;
+  #     };
+
+  #   Install.WantedBy = [ "default.target" ];
+  # };
 
 
 }
