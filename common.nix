0597e4daa38f7e108b55651c71b6211805308bad
Chris McDonough <chrism@plope.com>
2024-04-05 04:26:04 -0400

    add aplay

--- common.nix
+++ common.nix
@@ -449,6 +449,7 @@ in
     dvtranscode
     clinfo
     yt-dlp
+    alsa-utils # aplay
     # https://github.com/WolfangAukang/nur-packages/issues/9#issuecomment-1089072988
     # share/vdhcoapp/net.downloadhelper.coapp install --user
     #config.nur.repos.wolfangaukang.vdhcoapp
