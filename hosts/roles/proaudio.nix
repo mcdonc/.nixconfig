f0cef9aae093f0604a16df86505fb9af8379427a
Chris McDonough <chrism@plope.com>
2024-04-05 04:26:51 -0400

    enable wireplumber, optimize latency for UA-25

--- hosts/roles/proaudio.nix
+++ hosts/roles/proaudio.nix
@@ -38,21 +38,58 @@ in
   musnix.rtcqs.enable = true;
   musnix.alsaSeq.enable = true;
 
-  #services.pipewire.wireplumber.enable = true;
+  services.pipewire.wireplumber.enable = true;
 
   environment.variables = {
     # override musnix to add $HOME/.vst to LXVST_PATH (for yabridge)
     LXVST_PATH = "$HOME/.vst:$HOME/.lxvst:$HOME/.nix-profile/lib/lxvst:/run/current-system/sw/lib/lxvst";
   };
 
+  # Optimized for the Edirol UA-25 USB capture device
+  #
+  # See https://alsa.opensrc.org/Edirol_UA-25#Advanced_Alsa_configuration
+  # for info about what UA-25 switches do
+  #
+  # Edirol UA-25 must be set to ADVANCE, 48K to support a quantum of 64;
+  # if ADVANCE is off (or presumably if set to 44.1K; ADVANCE off implies 44.1K)
+  # there is crackling at quantums below 128, presumably due to the upscaling
+  # that pipewire does to 48K.  Crackling at 32 no matter what.
+
   environment.etc."pipewire/pipewire.conf.d/92-low-latency.conf" = {
     text = ''
       context.properties = {
-        default.clock.quantum = 32
+        default.clock.quantum = 64
+        default.clock.min-quantum = 64
+        default.clock.max-quantum = 64
       }
       jack.properties = {
-        node.quantum = 32/48000
+        node.quantum = 64/48000
       }
     '';
   };
+
+  # No settings at all seems better than any setting I've tried below
+
+  # environment.etc."wireplumber/main.lua.d/52-usb-ua25-config.lua" = {
+  #   text = ''
+  #     rule = {
+  #       matches = {
+  #         {
+  #           -- Matches all sources.
+  #           { "node.name", "matches", "alsa_input.usb-Roland_EDIROL_UA-25-00.*" },
+  #         },
+  #         {
+  #           -- Matches all sinks.
+  #           { "node.name", "matches", "alsa_output.usb-Roland_EDIROL_UA-25-00.*" },
+  #         },
+  #       },
+  #       apply_properties = {
+  #         ["api.alsa.period-size"]   = 256,
+  #         ["api.alsa.period-num"]    = 3,
+  #       },
+  #     }
+
+  #     table.insert(alsa_monitor.rules, rule)
+  #   '';
+  # };
 }
