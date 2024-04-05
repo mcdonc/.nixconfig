a1aac61d1757fb5e710e47ab8b20af9102f24812
Chris McDonough <chrism@plope.com>
2024-04-05 02:07:47 -0400

    low-latency for pipewire

--- hosts/roles/proaudio.nix
+++ hosts/roles/proaudio.nix
@@ -33,63 +33,26 @@ in
   ];
 
   # see https://github.com/musnix/musnix
-  # Activate the performance CPU frequency scaling governor.
-  # Set vm.swappiness to 10.
-  # Set the following udev rules (enable high-precision timers, if they exist):
-  #  KERNEL=="rtc0", GROUP="audio"
-  #  KERNEL=="hpet", GROUP="audio"
-  # Set the following PAM limits:
-  #  @audio  -       memlock unlimited
-  #  @audio  -       rtprio  99
-  #  @audio  soft    nofile  99999
-  #  @audio  hard    nofile  99999
-  # Set environment variables to default install locations in NixOS:
-  #  VST_PATH
-  #  VST3_PATH
-  #  LXVST_PATH
-  #  LADSPA_PATH
-  #  LV2_PATH
-  #  DSSI_PATH
-  # Allow users to install plugins in the following directories:
-  #  ~/.vst
-  #  ~/.vst3
-  #  ~/.lxvst
-  #  ~/.ladspa
-  #  ~/.lv2
-  #  ~/.dssi
-  # musnix.alsaSql.enable does
-  #  boot.kernelModules = [ "snd-seq" "snd-rawmidi" ];
-  # musnix.rtirq.enable
-  #  see https://wiki.linuxaudio.org/wiki/system_configuration
-
   musnix.enable = true;
   musnix.rtirq.enable = true;
   musnix.rtcqs.enable = true;
   musnix.alsaSeq.enable = true;
+
+  #services.pipewire.wireplumber.enable = true;
 
   environment.variables = {
     # override musnix to add $HOME/.vst to LXVST_PATH (for yabridge)
     LXVST_PATH = "$HOME/.vst:$HOME/.lxvst:$HOME/.nix-profile/lib/lxvst:/run/current-system/sw/lib/lxvst";
   };
 
-  environment.etc = let
-    json= pkgs.formats.json {};
-  in {
-    "pipewire/pipewire.d/92-low-latency.conf".source = json.generate
-      "92-low-latency.conf" {
-        context.properties = {
-          default.clock.rate = 48000;
-          default.clock.quantum = 32;
-          default.clock.min-quantum = 32;
-          default.clock.max-quantum = 32;
-        };
-      };
-    # this doesn't work
-    # "pipewire/jack.conf".source = json.generate "jack.conf" {
-    #   jack.properties = {
-    #     rt.prio = 99;
-    #     node.latency = "32/48000";
-    #     };
-    #   };
+  environment.etc."pipewire/pipewire.conf.d/92-low-latency.conf" = {
+    text = ''
+      context.properties = {
+        default.clock.quantum = 32
+      }
+      jack.properties = {
+        node.quantum = 32/48000
+      }
+    '';
   };
 }
