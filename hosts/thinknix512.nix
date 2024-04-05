f0c1ad961314fd7666d1ef96f1d5a1d8db373c1c
Chris McDonough <chrism@plope.com>
2024-04-05 06:40:30 -0400

    p512 is a backupsource, not sessile, no vmount

--- hosts/thinknix512.nix
+++ hosts/thinknix512.nix
@@ -6,13 +6,13 @@
     "${nixos-hardware}/lenovo/thinkpad/p51"
     "${nixos-hardware}/common/pc/ssd"
     ./roles/pseries.nix
-    ./roles/sessile.nix
+    #./roles/sessile.nix
     ./roles/encryptedzfs.nix
     ./roles/tlp.nix
-    # targeting 535.129.03, 545.29.02 backlightrestore doesn't work
     ./roles/dnsovertls/resolvedonly.nix
+    ./roles/backupsource
     ./roles/steam.nix
-    ./roles/vmount.nix
+    #./roles/vmount.nix
     ../common.nix
   ];
   system.stateVersion = "22.05";
@@ -25,22 +25,5 @@
   # silence ACPI "errors" at boot shown before NixOS stage 1 output
   # (default is 4)
   boot.consoleLogLevel = 3;
-
-  services.sanoid = {
-    enable = true;
-    interval = "*:2,32"; # run this more often than syncoid (every 30 mins)
-    datasets = {
-      "NIXROOT/home" = {
-        autoprune = true;
-        autosnap = true;
-        hourly = 1;
-        daily = 1;
-        weekly = 1;
-        monthly = 1;
-        yearly = 0;
-      };
-    };
-    extraArgs = [ "--debug" ];
-  };
 
 }
