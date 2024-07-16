02f92ad751e40d54459a3cacf340b76eaed7c91f
Chris McDonough <chrism@plope.com>
2024-07-16 16:39:40 -0400

    temporarily disable caches

--- pkgs/cachix/nixpkgs-python.nix
+++ pkgs/cachix/nixpkgs-python.nix
@@ -1,10 +1,10 @@
-{
-  nix = {
-    settings.substituters = [
-      "https://nixpkgs-python.cachix.org"
-    ];
-    settings.trusted-public-keys = [
-      "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
-    ];
-  };
-}
+# {
+#   nix = {
+#     settings.substituters = [
+#       "https://nixpkgs-python.cachix.org"
+#     ];
+#     settings.trusted-public-keys = [
+#       "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
+#     ];
+#   };
+# }
