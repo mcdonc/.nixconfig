f4d65c0f90f955e82c98818077959093fa0c4767
Chris McDonough <chrism@plope.com>
2024-07-16 13:28:24 -0400

    cifs config for keithmoon

--- hosts/keithmoon.nix
+++ hosts/keithmoon.nix
@@ -35,4 +35,60 @@
   services.xserver.enable = lib.mkForce false;
   services.displayManager.sddm.enable = lib.mkForce false;
 
+  environment.systemPackages = [ pkgs.cifs-utils ];
+
+  services.samba = {
+    enable = true;
+    package = pkgs.samba4Full;
+    syncPasswordsByPam = true;
+    openFirewall = true;
+    securityType = "user";
+    extraConfig = ''
+      workgroup = WORKGROUP
+      server string = keithmoon
+      netbios name = keithmoon
+      security = user
+      browseable = yes
+      smb encrypt = required
+      # note: localhost is the ipv6 localhost ::1
+      hosts allow = 192.168.1. 127.0.0.1 localhost
+      hosts deny = 0.0.0.0/0
+      guest account = nobody
+      map to guest = bad user
+    '';
+    shares = {
+      root = {
+        path = "/";
+        browseable = "yes";
+        writeable = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "chrism";
+        "force group" = "users";
+      };
+      homes = {
+        browseable = "no";  # note: each home will be browseable; the "homes" share will not.
+        "read only" = "no";
+        "guest ok" = "no";
+      };
+    };
+  };
+
+  avahi = {
+    publish.enable = true;
+    publish.userServices = true;
+    # ^^ Needed to allow samba to automatically register mDNS records (without the need for an `extraServiceFile`
+    #nssmdns4 = true;
+    # ^^ Not one hundred percent sure if this is needed- if it aint broke, don't fix it
+    enable = true;
+    openFirewall = true;
+  };
+
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+  };
+
 }
---
+++ hosts/roles/keithclient.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+
+let
+  host = "keithmoon";
+in
+
+{
+  # For mount.cifs, required unless domain name resolution is not needed.
+  environment.systemPackages = [ pkgs.cifs-utils ];
+  fileSystems."/keithmoon" = {
+    device = "//${host}/root";
+    fsType = "cifs";
+    options = [
+      "uid=chrism"
+      "gid=users"
+      "x-systemd.automount"
+      "x-systemd.idle-timeout=60"
+      "x-systemd.device-timeout=5s"
+      "x-systemd.mount-timeout=5s"
+      "noauto"
+      "credentials=/etc/smb-secrets"
+    ];
+  };
+}
