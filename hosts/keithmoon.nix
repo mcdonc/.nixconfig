adb7caf944bab6f282e9d776572b6d773886d77f
Chris McDonough <chrism@plope.com>
2024-07-16 15:07:44 -0400

    cifs config for keithmoon

--- hosts/keithmoon.nix
+++ hosts/keithmoon.nix
@@ -69,9 +69,12 @@
         "force group" = "users";
       };
       homes = {
-        browseable = "no";  # note: each home will be browseable; the "homes" share will not.
+        browseable = "no";
+        # note: each home will be browseable; the "homes" share will not.
         "read only" = "no";
         "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
       };
     };
   };
@@ -79,9 +82,11 @@
   avahi = {
     publish.enable = true;
     publish.userServices = true;
-    # ^^ Needed to allow samba to automatically register mDNS records (without the need for an `extraServiceFile`
+    # ^^ Needed to allow samba to automatically register mDNS records
+    # (without the need for an `extraServiceFile`
     #nssmdns4 = true;
-    # ^^ Not one hundred percent sure if this is needed- if it aint broke, don't fix it
+    # ^^ Not one hundred percent sure if this is needed- if it aint broke,
+    # don't fix it
     enable = true;
     openFirewall = true;
   };
