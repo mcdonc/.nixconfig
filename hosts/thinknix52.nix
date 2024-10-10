01fc860daadfa84d881c1e99b180c4a0850da38f
Chris McDonough <chrism@plope.com>
2024-10-10 08:57:58 -0400

    use normal dns

--- hosts/thinknix52.nix
+++ hosts/thinknix52.nix
@@ -12,7 +12,7 @@
     ./roles/steam.nix
     ./roles/davinci-resolve/studio.nix
 #    ./roles/vmount.nix  # no steam when this is enabled, but nec for dvresolve
-    ./roles/dnsovertls/resolvedonly.nix
+#    ./roles/dnsovertls/resolvedonly.nix
     ./roles/backupsource
     ../common.nix
   ];
