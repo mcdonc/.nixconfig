70bb284bce95572b1731738653f5d9d13f56fa20
Chris McDonough <chrism@plope.com>
2024-10-30 02:35:11 +0000

    80 cols

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -208,16 +208,16 @@ in
         # https://medium.com/@dannysmith/little-thing-2-speeding-up-zsh-f1860390f92
         # (needs extended_glob)
         completionInit = ''
-          # On slow systems, checking the cached .zcompdump file to see if it must
-          # be regenerated adds a noticable delay to zsh startup.  This little
-          # hack restricts it to once a day.  It should be pasted into your own
-          # completion file.
+          # On slow systems, checking the cached .zcompdump file to see if it
+          # must be regenerated adds a noticable delay to zsh startup.  This
+          # little hack restricts it to once a day.  It should be pasted into
+          # your own completion file.
           #
           # The globbing is a little complicated here:
           # - '#q' is an explicit glob qualifier that makes globbing work within
           #   zsh's [[ ]] construct.
-          # - 'N' makes the glob pattern evaluate to nothing when it doesn't match
-          #   (rather than throw a globbing error)
+          # - 'N' makes the glob pattern evaluate to nothing when it doesn't
+          #   match (rather than throw a globbing error)
           # - '.' matches "regular files"
           # - 'mh+24' matches files (or directories or whatever) that are older
           #   than 24 hours.
@@ -282,7 +282,8 @@ in
         plugins = [
           {
             name = "fast-syntax-highlighting";
-            src = "${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions";
+            src =
+              "${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions";
           }
         ];
       };
