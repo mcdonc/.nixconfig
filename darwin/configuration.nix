f5024e0d573c6ebf59f3d39619f548feb344653e
Chris McDonough <chrism@plope.com>
2024-10-30 05:06:02 +0000

    docs

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -67,7 +67,6 @@ in
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
-  # nix.package = pkgs.nix;
 
   # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
