e2489c57dae6a6ff999976c735487fce96281006
Chris McDonough <chrism@plope.com>
2025-01-07 00:19:04 -0500

    disable nix-index

--- users/chrism/home.nix
+++ users/chrism/home.nix
@@ -115,33 +115,33 @@ in
   };
 
   # systemctl --user status nix-index.service
-  systemd.user.services.nix-index = {
-    Unit = {
-      Description = "Run nix-index.";
-    };
-    Service = {
-      Type = "oneshot";
-      ExecStart = "${pkgs.nix-index}/bin/nix-index";
-    };
-    Install = {
-      WantedBy = [ "default.target" ];
-    };
-  };
+  # systemd.user.services.nix-index = {
+  #   Unit = {
+  #     Description = "Run nix-index.";
+  #   };
+  #   Service = {
+  #     Type = "oneshot";
+  #     ExecStart = "${pkgs.nix-index}/bin/nix-index";
+  #   };
+  #   Install = {
+  #     WantedBy = [ "default.target" ];
+  #   };
+  # };
 
-  # systemctl --user status nix-index.timer
-  systemd.user.timers.nix-index = {
-    Unit = {
-      Description = "Timer for nix-index.";
-    };
-    Timer = {
-      Unit = "nix-index.service";
-      #OnCalendar = "*:0/5";
-      OnCalendar = "*-*-* 10:00:00";
-    };
-    Install = {
-      WantedBy = [ "timers.target" ];
-    };
-  };
+  # # systemctl --user status nix-index.timer
+  # systemd.user.timers.nix-index = {
+  #   Unit = {
+  #     Description = "Timer for nix-index.";
+  #   };
+  #   Timer = {
+  #     Unit = "nix-index.service";
+  #     #OnCalendar = "*:0/5";
+  #     OnCalendar = "*-*-* 10:00:00";
+  #   };
+  #   Install = {
+  #     WantedBy = [ "timers.target" ];
+  #   };
+  # };
 
   # systemd.user.services.watchintake = {
   #   Unit = {
