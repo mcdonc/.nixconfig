4038a86d5e5f1e7d3097d00a38f97d099b5ba874
Chris McDonough <chrism@plope.com>
2025-02-03 00:52:55 -0500

    map right alt+pgup to home and right alt+pgdn to end

--- common.nix
+++ common.nix
@@ -131,11 +131,18 @@ in
       ! pointer = 1 0 3 4 5
       ! map right-ctrl+arrow-keys to pgup/pgdn/home/end
       ! see https://forums.linuxmint.com/viewtopic.php?t=321400
+      ! keycode <number> = <default> <shift> <alt> <alt+shift>
+      ! keycode 105 is right-ctrl
       keycode 105 = Mode_switch
       keycode 113 = Left NoSymbol Home
       keycode 114 = Right NoSymbol End
       keycode 111 = Up NoSymbol Prior
       keycode 116 = Down NoSymbol Next
+      ! map alt-pgup to home and alt-pgdn to end
+      ! keycode 108 is right-alt
+      keycode 108 = Mode_switch
+      keycode 112 = Prior NoSymbol Home
+      keycode 117 = Next NoSymbol End
     '';
     in "${pkgs.xorg.xmodmap}/bin/xmodmap ${modmap}";
 
