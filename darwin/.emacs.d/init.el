ebdcec226aedbce7ddda926acd95543cd5b19ba6
Chris McDonough <chrism@plope.com>
2025-03-25 07:55:14 -0400

    add terramate stuff

--- darwin/.emacs.d/init.el
+++ darwin/.emacs.d/init.el
@@ -254,6 +254,9 @@
 (add-to-list 'auto-mode-alist '("\\.jinja2$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
 
+(add-to-list 'auto-mode-alist '("\\.tmgen$" . hcl-mode))
+(add-to-list 'auto-mode-alist '("\\.hcl$" . hcl-mode))
+
 
 (defun word-count nil "Count words in buffer." (interactive)
   (shell-command-on-region (point-min) (point-max) "wc -w"))
