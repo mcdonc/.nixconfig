905903f22c02a54a81151b4f3b3cf6b5fb8e0518
Chris McDonough <chrism@plope.com>
2025-05-18 19:11:16 -0400

    add thinknix50 as peerix

--- hosts/roles/peerix.nix
+++ hosts/roles/peerix.nix
@@ -7,7 +7,7 @@
     enable = true;
     privateKeyFile = "/etc/nixos/peerix-private";
     publicKeyFile = "/etc/nixos/peerix-public";
-    publicKey = "peerix-keithmoon:s/DzPd1deGneuvQsCT1FzgyoXghuOtNt8pLDOM3qko0=";
+    publicKey = "peerix-keithmoon:s/DzPd1deGneuvQsCT1FzgyoXghuOtNt8pLDOM3qko0= peerix-thinknix50:ep42Cnw+QZj84FT5lb6pHOmKMIL+XWQViJBevVCZx1o=";
   };
 
 }
