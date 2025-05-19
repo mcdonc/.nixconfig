64b9b5980cd35b0498a25896287e8282c13c8995
Chris McDonough <chrism@plope.com>
2025-05-19 01:23:58 -0400

    remove flox

--- common.nix
+++ common.nix
@@ -68,8 +68,6 @@ in
       auto-optimise-store = true;
       experimental-features = "nix-command flakes";
       trusted-users = [ "root" "@wheel" ];
-      trusted-substituters = [ "https://cache.flox.dev" ];
-      trusted-public-keys = [ "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs=" ];
       #sandbox = "relaxed";
     };
     gc = {
