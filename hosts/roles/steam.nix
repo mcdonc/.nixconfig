2f7287636c88d482d43e99fa58f6c4fdf8b5dd77
Chris McDonough <chrism@plope.com>
2025-05-21 00:38:29 -0400

    get rid of nix-gaming

--- hosts/roles/steam.nix
+++ hosts/roles/steam.nix
@@ -6,11 +6,4 @@
     enable = true;
     extraCompatPackages = with pkgs; [ proton-ge-bin ];
   };
-  nix.settings = {
-    substituters = [ "https://nix-gaming.cachix.org" ];
-    trusted-public-keys = [
-      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-    ];
-  };
-
 }
