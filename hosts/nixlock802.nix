12ba2f2b077ca855268239534b0d1fe07c196e9b
Chris McDonough <chrism@plope.com>
2025-05-27 00:27:38 -0400

    make wifi routing work

--- hosts/nixlock802.nix
+++ hosts/nixlock802.nix
@@ -11,6 +11,10 @@
 
   services.doorclient.enable = true;
 
+
+  hardware.enableAllFirmware = true;
+  hardware.firmware = [ pkgs.raspberrypiWirelessFirmware ];
+
   boot.initrd.availableKernelModules = [ "xhci_pci" "usbhid" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
@@ -35,36 +39,47 @@
   networking.hostName = "nixlock802";
   networking.enableIPv6 = false;
   networking.firewall.enable = lib.mkForce false;
-  networking.networkmanager.enable = lib.mkForce false;
-  networking.wireless.enable = true;
+  networking.networkmanager.enable = lib.mkForce true;
+
+  # networkmanager try connect
+  # sudo nmcli device wifi connect "ssid" password "password"
+
+  # to use wpa_supplicant, disable networkmanager and add this (routing
+  # doesnt work on wifi)
+
+  #networking.wireless.enable = true;
   #networking.wireless.secretsFile = "/var/lib/secrets/wifi";
   #networking.wireless.networks.haircut.pskRaw = "ext:psk";
-  networking.wireless.networks.ytvid-rpi.pskRaw = "18a90748cff3ae6006b78dc2b4a65be47f7e8eb22c46388b636314b535486dcb";
+  #networking.wireless.networks.ytvid-rpi.pskRaw = "18a90748cff3ae6006b78dc2b4a65be47f7e8eb22c46388b636314b535486dcb";
 
   # "wpa_passphrase ssid passphrase" creates a psk
   # "iwconfig" shows connected ssids
 
-  networking = {
-    interfaces.end0 = {
-      ipv4.addresses = [{
-        address = "192.168.1.185";
-        prefixLength = 24;
-      }];
-    };
-    defaultGateway = {
-      address = "192.168.1.1";
-      interface = "end0";
-    };
-    nameservers = [
-      "192.168.1.1"
-    ];
-  };
+  #networking = {
+  #  interfaces.end0 = {
+  #    ipv4.addresses = [{
+  #      address = "192.168.1.185";
+  #      prefixLength = 24;
+  #    }];
+  #  };
+  #  defaultGateway = {
+  #    address = "192.168.1.1";
+  #    interface = "end0";
+  #  };
+  #  nameservers = [
+  #    "192.168.1.1"
+  #  ];
+  #};
+
+  # end wpa_supplicant
 
   environment.systemPackages = [
     pkgs.usbutils # lsusb
     pkgs.pciutils # lsusb
     pkgs.wirelesstools # iwconfig
     pkgs.wpa_supplicant
+    pkgs.networkmanager
+
   ];
 
 }
