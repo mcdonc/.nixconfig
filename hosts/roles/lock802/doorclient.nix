849ac2d9cf1ac71f4c7dbf6660e74ab5939aca05
Chris McDonough <chrism@plope.com>
2025-06-01 00:10:46 -0400

    chart

--- hosts/roles/lock802/doorclient.nix
+++ hosts/roles/lock802/doorclient.nix
@@ -91,11 +91,15 @@ in {
   #o   o   o   o   o   o   o   o   o   o   o   o   o   o   o   o   o   o   o   o
   #G  26      13               G                               G             3.3
   #   FD
+  #  BRN
   #
   #o   o   o   o   o   o   o   o   o   o   o   o   o   o   o   o   o   o   o   o
-  #       16   G                           G  24                       G      5V  #       CB
+  #       16   G                           G  24                       G      5V  #       CB                                  ID
+  #       WH  BLU                            GRN
   #
   # relay sitting on bottom of box is for front door unlock: brown to 26
+  # relay stuck to door on left is inner door unlock: green to 24
+  # relay stuck to door on right is for callbutton
 
 
   config = let
