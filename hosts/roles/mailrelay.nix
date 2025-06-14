37acad1c7adec395161b4e763382f623f47fab5c
Chris McDonough <chrism@plope.com>
2025-06-14 04:32:53 -0400

    finalize zed alerts

--- hosts/roles/mailrelay.nix
+++ hosts/roles/mailrelay.nix
@@ -10,17 +10,17 @@
     let
       passwordfile = config.age.secrets."chris-mail-sasl".path;
     in
-      {
-        enable = true;
-        relayHost = "arctor.repoze.org";
-        relayPort = 587;
-        config = {
-          smtp_use_tls = "yes";
-          smtp_sasl_auth_enable = "yes";
-          smtp_sasl_security_options = "";
-          smtp_sasl_password_maps = "texthash:${passwordfile}";
-          # Forward mails to root (e.g. from cron jobs, smartd, etc)
-          virtual_alias_maps = "inline:{ {root=chrism@repoze.org } }";
-        };
+    {
+      enable = true;
+      relayHost = "arctor.repoze.org";
+      relayPort = 587;
+      config = {
+        smtp_use_tls = "yes";
+        smtp_sasl_auth_enable = "yes";
+        smtp_sasl_security_options = "";
+        smtp_sasl_password_maps = "texthash:${passwordfile}";
+        # Forward mails to root (e.g. from cron jobs, smartd, etc)
+        virtual_alias_maps = "inline:{ {root=chrism@repoze.org } }";
       };
+    };
 }
--- hosts/roles/zedalerts.nix
+++ hosts/roles/zedalerts.nix
@@ -1,14 +1,30 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
+let
+  zedtest = pkgs.writeShellScriptBin "zedtest" ''
+    dd if=/dev/zero of=/tmp/sparse_file bs=1 count=0 seek=512M
+    sudo zpool create zedtest /tmp/sparse_file
+    sudo zpool scrub zedtest
+    sudo zpool export zedtest
+    rm /tmp/sparse_file
+  '';
+in
 {
+  environment.systemPackages = [
+    zedtest
+  ];
+
   nixpkgs.config.packageOverrides = pkgs: {
     zfs = pkgs.zfs.override {
       enableMail = true;
     };
   };
+
+  services.zfs.zed.enableMail = true;
   services.zfs.zed.settings = {
     ZED_EMAIL_ADDR = [ "chrism@repoze.org" ];
-    ZED_EMAIL_OPTS = "-s '@SUBJECT@' -r noreply@repoze.org @ADDRESS@";
+    ZED_EMAIL_PROG = "${pkgs.mailutils}/bin/mail";
+    ZED_EMAIL_OPTS = "-s '@SUBJECT@' -r chrism@repoze.org @ADDRESS@";
     ZED_NOTIFY_VERBOSE = true;
   };
 
