b8407fa99bffba79512fd0f486a12395f0f174c9
Chris McDonough <chrism@plope.com>
2025-06-14 00:51:50 -0400

    nixfmt

--- hosts/roles/tailscale/default.nix
+++ hosts/roles/tailscale/default.nix
@@ -10,8 +10,14 @@
     description = "Automatic connection to Tailscale";
 
     # make sure tailscale is running before trying to connect to tailscale
-    after = [ "network-pre.target" "tailscale.service" ];
-    wants = [ "network-pre.target" "tailscale.service" ];
+    after = [
+      "network-pre.target"
+      "tailscale.service"
+    ];
+    wants = [
+      "network-pre.target"
+      "tailscale.service"
+    ];
     wantedBy = [ "multi-user.target" ];
 
     # set this service as a oneshot job
