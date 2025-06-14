21f9cd34817055727ee401ae1ef6fa847bcc3054
Chris McDonough <chrism@plope.com>
2025-06-14 14:35:28 -0400

    smh

--- hosts/roles/logcheck.nix
+++ hosts/roles/logcheck.nix
@@ -3,7 +3,7 @@ let
   lcjc = pkgs.writeShellScriptBin "logcheck-journalctl" ''
     journalctl -o short-iso --since "$(date -d '1 day ago' '+%Y-%m-%d %H:%M:%S')"
   '';
-  hour = "14";
+  hour = "15";
   tmpfile = "/tmp/lcjc.txt";
 in
 {
@@ -14,10 +14,10 @@ in
     });
   })];
 
-  # all this nonsense is needed because logcheck is unmaintained and packaged poorly
+  # all this tmpfile nonsense is needed because logcheck is unmaintained and packaged poorly
   services.cron.systemCronJobs = [
     "0 ${hour} * * * logcheck ${lcjc}/bin/logcheck-journalctl > ${tmpfile} 2>&1"
-  ];
+  ]; # logcheck itself will run at ${hour}:02, the tmpfile creation will run at ${hour}:00
   services.logcheck.enable = true;
   services.logcheck.timeOfDay = hour;
   services.logcheck.level = lib.mkDefault "workstation";
