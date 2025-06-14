16dac84b633695f464467c6b16fba02ba70f2522
Chris McDonough <chrism@plope.com>
2025-06-14 13:55:18 -0400

    logcheck nonsense

--- hosts/roles/logcheck.nix
+++ hosts/roles/logcheck.nix
@@ -1,4 +1,11 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
+let
+  lcjc = pkgs.writeShellScriptBin "logcheck-journalctl" ''
+    journalctl -o short-iso --since "$(date -d '1 day ago' '+%Y-%m-%d %H:%M:%S')"
+  '';
+  hour = "14";
+  tmpfile = "/tmp/lcjc.txt";
+in
 {
   # see https://github.com/NixOS/nixpkgs/issues/240383
   nixpkgs.overlays = [(self: super: {
@@ -7,8 +14,16 @@
     });
   })];
 
+  # all this nonsense is needed because logcheck is unmaintained and packaged poorly
+  services.cron.systemCronJobs = [
+    "0 ${hour} * * * logcheck ${lcjc}/bin/logcheck-journalctl > ${tmpfile} 2>&1"
+  ];
   services.logcheck.enable = true;
+  services.logcheck.timeOfDay = hour;
   services.logcheck.level = lib.mkDefault "workstation";
-  #services.logcheck.mailTo = "root";
+  services.logcheck.files = lib.mkDefault [
+    tmpfile
+  ];
+  services.logcheck.mailTo = lib.mkDefault "root";
 
 }
