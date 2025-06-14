ba114698034ee9150e1427efef345b51398e56f8
Chris McDonough <chrism@plope.com>
2025-06-14 00:44:02 -0400

    nixfmt

--- hosts/clonelock802.nix
+++ hosts/clonelock802.nix
@@ -1,7 +1,14 @@
-{ pkgs, lib, inputs, pkgs-gpio, config, ... }:
+{
+  pkgs,
+  lib,
+  inputs,
+  pkgs-gpio,
+  config,
+  ...
+}:
 
 {
-  imports = [./lock802.nix];
+  imports = [ ./lock802.nix ];
   networking.hostId = lib.mkForce "f034f642";
   networking.hostName = lib.mkForce "clonelock802";
   services.doorclient.clientidentity = lib.mkForce "clonedoorclient";
