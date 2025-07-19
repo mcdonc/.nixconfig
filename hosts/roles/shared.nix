33f215061f3712ee118f807b73374e8b7c0c60d8
Chris McDonough <chrism@plope.com>
2025-07-19 08:50:22 -0400

    enable nix-ld

--- hosts/roles/shared.nix
+++ hosts/roles/shared.nix
@@ -85,4 +85,11 @@
 
   users.groups.nixconfig = { };
 
+  # enable nix-ld for pip and uv and friends
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    stdenv.cc.cc.lib
+    zlib # numpy
+  ];
+
 }
