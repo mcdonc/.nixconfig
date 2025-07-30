d42bc210b62dd95599e059adbf97861e4ccf13af
Chris McDonough <chrism@plope.com>
2025-07-30 00:04:04 -0400

    failure case

--- hosts/roles/enfold/rag.nix
+++ hosts/roles/enfold/rag.nix
@@ -32,8 +32,8 @@
       mkdir -p "$RAGENV_DIR/tmp"
       export TMPDIR="$RAGENV_DIR/tmp" # we run out of space on /tmp via pip
       curl -X POST --data-urlencode "payload={\"channel\": \"#afsoc-rag\", \"username\": \"nixbot\", \"text\": \"rag.repoze.org processes starting\", \"icon_emoji\": \":ghost:\"}" "$SLACK_NOTIFY_URL"
-      $DEVENV_CMD shell -- flutterbuildandrunweb
-      curl -X POST --data-urlencode "payload={\"channel\": \"#afsoc-rag\", \"username\": \"nixbot\", \"text\": \"rag.repoze.org processes stopped\", \"icon_emoji\": \":ghost:\"}" "$SLACK_NOTIFY_URL"
+      $DEVENV_CMD shell -- flutterbuildandrunweb || \
+      curl -X POST --data-urlencode "payload={\"channel\": \"#afsoc-rag\", \"username\": \"nixbot\", \"text\": \"rag.repoze.org processes did not start\", \"icon_emoji\": \":face_with_symbols_on_mouth:\"}" "$SLACK_NOTIFY_URL"
     '';
     serviceConfig = {
       Restart = "always";
