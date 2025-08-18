fdc7e4059b7e178e338dc59dc99951b66ec7b820
Chris McDonough <chrism@plope.com>
2025-08-18 00:54:15 -0400

    secrets work

--- hosts/roles/enfold/age.nix
+++ hosts/roles/enfold/age.nix
@@ -71,14 +71,14 @@
   };
 
   age.secrets."gandi-api" = {
-    file = ../secrets/gandi-api.age;
+    file = ../../../secrets/gandi-api.age;
     mode = "640";
     owner = "root";
     group = "acme";
   };
 
   age.secrets."enfold-alan-pat" = {
-    file = ../secrets/enfold-alan-pat.age;
+    file = ../../../secrets/enfold-alan-pat.age;
     mode = "640";
     owner = "root";
     group = "users";
