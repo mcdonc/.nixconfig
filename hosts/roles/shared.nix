3957ed64b4e72e972ebe53d099594d0b4caf33fb
Chris McDonough <chrism@plope.com>
2025-09-15 14:15:17 -0400

    cuda from flox

--- hosts/roles/shared.nix
+++ hosts/roles/shared.nix
@@ -25,6 +25,9 @@
         "root"
         "@wheel"
       ];
+      # CUDA
+      extra-trusted-substituters = [ "https://cache.flox.dev" ];
+      extra-trusted-public-keys = [ "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs=" ];
     };
     gc = {
       automatic = true;
