050e2a481aae54b99ad32db622fdefca1db33c6d
Chris McDonough <chrism@plope.com>
2026-01-07 00:13:48 -0500

    comment

--- hosts/roles/nvidiapassthru.nix
+++ hosts/roles/nvidiapassthru.nix
@@ -16,6 +16,7 @@
   virtualisation.docker.enableNvidia = true;
   # ^^ deprecated but necessary 4 solplx
   # see also https://github.com/NixOS/nixpkgs/pull/344188
+  # and https://discourse.nixos.org/t/nvidia-docker-container-runtime-doesnt-detect-my-gpu/51336/4
   virtualisation.docker.enable = true;
 }
   
