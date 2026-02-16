3fe6e553b2ab59f2f2a89245d5f5f7ce09139dfb
Chris McDonough <chrism@plope.com>
2026-02-16 16:52:48 -0500

    systemPackage to systemPackages

--- pkgs/rtcqs.nix
+++ pkgs/rtcqs.nix
@@ -1,25 +1,23 @@
-{pkgs, fetchPypi, ... }:
+{ pkgs, fetchPypi, ... }:
 
 {
-  environment.systemPackage = [
+  environment.systemPackages = [
 
-    (
-      pkgs.python3.pkgs.buildPythonApplication rec {
-        pname = "rtcqs";
-        version = "0.6.2";
-        format = "pyproject";
-        buildInputs = [
-          pkgs.python3.pkgs.setuptools
-        ];
-        propagatedBuildInputs = [
-          pkgs.python3.pkgs.pysimplegui
-        ];
-        src = fetchPypi {
-          inherit pname version;
-          hash = "sha256-DfeV9kGhdMf6hZ1iNJ0L3HUn7m8c1gRK5cjtJNUAvJI=";
-        };
-      }
-    )
+    (pkgs.python3.pkgs.buildPythonApplication rec {
+      pname = "rtcqs";
+      version = "0.6.2";
+      format = "pyproject";
+      buildInputs = [
+        pkgs.python3.pkgs.setuptools
+      ];
+      propagatedBuildInputs = [
+        pkgs.python3.pkgs.pysimplegui
+      ];
+      src = fetchPypi {
+        inherit pname version;
+        hash = "sha256-DfeV9kGhdMf6hZ1iNJ0L3HUn7m8c1gRK5cjtJNUAvJI=";
+      };
+    })
 
   ];
 
