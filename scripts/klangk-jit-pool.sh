e585b93bdfb22736bec4a41eb807483655d1c1fd
Chris McDonough <chrism@plope.com>
2026-08-17 14:48:11 -0400

    klangk-jit-pool: reduce hard timeout to 60 min

--- scripts/klangk-jit-pool.sh
+++ scripts/klangk-jit-pool.sh
@@ -19,7 +19,7 @@ LABEL="nix"
 STATE=/var/lib/klangk-jit-pool
 RUN=/run/klangk-jit-pool
 MAX_VMS=4
-JOB_TIMEOUT_SECS=5400 # 90 min hard kill per job VM
+JOB_TIMEOUT_SECS=3600 # 60 min hard kill per job VM
 POLL_SECS=20
 
 VM_RUN="@vmClosure@/bin/run-klangk-jit-vm"
